3e8f118f8de422db71e6247df9a559e0b41d587e
okta26-hub <55014261+okta26-hub@users.noreply.github.com>
2023-03-15 16:25:29 +0700

    QA_Automation_Testing

---
+++ Kalkulator Faktorial Python X Selenium/TC_14_Input _Billangan_Desimal.py
@@ -0,0 +1,31 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+class Test_Input(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+ 
+    def test_input(self):
+        # steps
+        self.driver.get("https://qa.putraprima.id/") # buka situs
+        time.sleep(3)
+        self.driver.find_element(By.CSS_SELECTOR, "#input").send_keys("10.5") # isi angka
+        time.sleep(1)
+        self.driver.find_element(By.CSS_SELECTOR, "#hitung").click() # klik button
+        time.sleep(3)    
+        
+        #validasi
+
+        expected_result = "Please enter an integer"
+        actual_result = self.driver.find_element(By.ID, "result").text
+        self.assertEqual(actual_result, expected_result, msg="Hasil tidak sesuai dengan harapan")
+        print("Hasil Testing Sesuai Dengan Test Case Yang Dikerjakan")
+
+    def tearDown(self):
+        self.driver.quit() # tutup koneksi socket pada akhir pengujian
+        
+if __name__ == "__main__":
+    unittest.main()
---
+++ Kalkulator Faktorial Python X Selenium/TC_9_Privacy_Policy.py
@@ -0,0 +1,26 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+class Test_Input(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+ 
+    def test_input(self):
+        # steps
+        self.driver.get("https://qa.putraprima.id/") # buka situs
+        time.sleep(3)
+        self.driver.find_element(By.CSS_SELECTOR, "body > div > div > div > div:nth-child(2) > a:nth-child(2)").click() # klik button
+        time.sleep(3)    
+        
+        #validasi
+        
+        print("Hasil Testing Sesuai Dengan Test Case Yang Dikerjakan")
+    
+    def tearDown(self):
+        self.driver.quit() # tutup koneksi socket pada akhir pengujian
+
+if __name__ == "__main__":
+    unittest.main()
